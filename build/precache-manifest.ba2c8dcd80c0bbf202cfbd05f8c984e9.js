7c8d9d4250a4f00f16d08d3584308e7d764caa87
Pius E. Uduma <41404729+controlpee@users.noreply.github.com>
2020-12-29 19:58:36 +0100

    Intial commit

---
+++ build/precache-manifest.ba2c8dcd80c0bbf202cfbd05f8c984e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "758b7fdac5199a55bd3d8fcdafc0078d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc98c0f535ce911a79a2",
+    "url": "/static/css/main.4f3e8cdb.chunk.css"
+  },
+  {
+    "revision": "23656dd7fe009e694edc",
+    "url": "/static/js/2.2fde01d5.chunk.js"
+  },
+  {
+    "revision": "fc98c0f535ce911a79a2",
+    "url": "/static/js/main.f37c6d65.chunk.js"
+  },
+  {
+    "revision": "c4d9d2f79292138ad86d",
+    "url": "/static/js/runtime-main.292bdbd0.js"
+  },
+  {
+    "revision": "5a021ab95b4d2a0c8e7511c40ca7bb2f",
+    "url": "/static/media/no-img.5a021ab9.png"
+  }
+]);
